432db5fe00c0811eb431afd9ea9a78f1980b5a22
Steve Nguyen <steve.nguyen.000@gmail.com>
2015-04-08 15:27:40 +0200

    fix misplaced hardwareinterface in urdf

--- poppy_gazebo/src/PoppySimpleROSMove.py
+++ poppy_gazebo/src/PoppySimpleROSMove.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-########################################################################
+#
 #  File Name	: 'PoppySimpleROSMove.py'
 #  Author	: Steve NGUYEN
 #  Contact      : steve.nguyen@inria.fr
@@ -15,13 +15,13 @@
 #
 #
 #  Notes:	notes
-########################################################################
+#
 
 from math import radians
 import time
 
 
-#ros stuff
+# ros stuff
 
 import roslib
 import rospy
@@ -30,20 +30,17 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64
 
 
-
-
-
-
-
 class Poppy():
+
     def __init__(self):
 
         rospy.init_node("poppy_controller")
 
-        self.joints_state_sub = rospy.Subscriber("/poppy/joint_states", JointState, self.state_cb)
+        self.joints_state_sub = rospy.Subscriber(
+            "/poppy/joint_states", JointState, self.state_cb)
 
-        #retrieve the joints names loaded in ROS
-        #they are of the form: l_hip_x_position_controller
+        # retrieve the joints names loaded in ROS
+        # they are of the form: l_hip_x_position_controller
 
         self.poppy_param = rospy.get_param("/poppy")
         self.joints_ctrl_name = []
@@ -52,11 +49,12 @@ class Poppy():
         self.joints_state = {}
 
         for k, d in self.poppy_param.iteritems():
-            if k != 'joint_state_controller': #just ignore this one
+            if k != 'joint_state_controller':  # just ignore this one
                 self.joints_ctrl_name.append(k)
                 self.joints_ctrl_pos.append(0.0)
-                #for each joint we create a publisher
-                self.joints_pub[k.replace('_position_controller', '')] = rospy.Publisher("/poppy/" + k + "/command", Float64)
+                # for each joint we create a publisher
+                self.joints_pub[k.replace('_position_controller', '')] = rospy.Publisher(
+                    "/poppy/" + k + "/command", Float64)
 
     def zero(self):
         for k, d in self.joints_pub.iteritems():
@@ -66,7 +64,8 @@ class Poppy():
 
     def state_cb(self, data):
         for i, n in enumerate(data.name):
-            self.joints_state[n] = data.position[i] #there is also velocity and effort
+            self.joints_state[n] = data.position[
+                i]  # there is also velocity and effort
 
     def move(self):
         while not rospy.is_shutdown():
@@ -75,8 +74,7 @@ class Poppy():
 
             time.sleep(0.1)
 
-if __name__ ==  '__main__':
-
+if __name__ == '__main__':
 
     poppy = Poppy()
     poppy.zero()
